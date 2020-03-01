34632f85ccfa69acf1aa19a7d2c3c72ee57a4abe
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-03-01 13:05:56 +0100

    Creating OpenGL context - small optimizations.

--- src/video/src/Context_Linux.cpp
+++ src/video/src/Context_Linux.cpp
@@ -10,13 +10,32 @@
 #include "glad/glad.h"
 #include "logger/log.h"
 
-bool Video::setupGL() {
+bool Video::setupGL()
+{
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
         log_error("SDL_Init Error: %s");
         SDL_ClearError();
         return false;
     }
+
+    SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 5 );
+    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 5 );
+    SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 5 );
+
+    SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 0);
+    SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 0);
+    SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE, 0);
+
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_RED_SIZE, 0);
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_GREEN_SIZE, 0);
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_BLUE_SIZE, 0);
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_ALPHA_SIZE, 0);
+
+    SDL_GL_SetAttribute( SDL_GL_ACCELERATED_VISUAL, 1);
+
+    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
 
     //  Create a window
     auto surface = SDL_SetVideoMode(getWindowWidth(),
@@ -24,13 +43,15 @@ bool Video::setupGL() {
                                     0, // current display's bpp
                                     SDL_DOUBLEBUF | SDL_OPENGL | SDL_SWSURFACE);
 
-    if (!surface) {
+    if (!surface)
+    {
         log_error("SDL_SetVideoMode Error: %s", SDL_GetError());
         SDL_ClearError();
         return false;
     }
 
-    if(!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress))
+    {
         log_error("Error while loading ptrs to OpenGL functions");
         return false;
     }
@@ -38,8 +59,31 @@ bool Video::setupGL() {
     DebugGlCall(glEnable(GL_TEXTURE_2D));
     DebugGlCall(glShadeModel(GL_SMOOTH));
     DebugGlCall(glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE));
-    DebugGlCall(glEnable(GL_BLEND));
-    DebugGlCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+    DebugGlCall(glEnableClientState(GL_VERTEX_ARRAY)); // For glVertexPointer
+    DebugGlCall(glEnableClientState(GL_TEXTURE_COORD_ARRAY)); // For glTexCoordPointer
+
+    // Disable, as it is not used and may affect performance:
+    DebugGlCall(glDisableClientState(GL_COLOR_ARRAY));
+    DebugGlCall(glDisableClientState(GL_EDGE_FLAG_ARRAY));
+    DebugGlCall(glDisableClientState(GL_NORMAL_ARRAY));
+    DebugGlCall(glDisableClientState(GL_INDEX_ARRAY));
+
+    DebugGlCall(glDisable(GL_FOG));
+    DebugGlCall(glDisable(GL_LIGHTING));
+    DebugGlCall(glDisable(GL_CULL_FACE));
+    DebugGlCall(glDisable(GL_ALPHA_TEST));
+    DebugGlCall(glDisable(GL_BLEND));
+    DebugGlCall(glDisable(GL_COLOR_LOGIC_OP));
+    DebugGlCall(glDisable(GL_DITHER));
+    DebugGlCall(glDisable(GL_STENCIL_TEST));
+    DebugGlCall(glDisable(GL_DEPTH_TEST));
+    DebugGlCall(glDisable(GL_POINT_SMOOTH));
+    DebugGlCall(glDisable(GL_LINE_SMOOTH));
+    DebugGlCall(glDisable(GL_SCISSOR_TEST));
+    DebugGlCall(glDisable(GL_COLOR_MATERIAL));
+    DebugGlCall(glDisable(GL_NORMALIZE));
+    DebugGlCall(glDisable(GL_RESCALE_NORMAL));
 
     log_info("Exiting Video::setupGL, success.");
     return true;
--- src/video/src/Context_PSP.cpp
+++ src/video/src/Context_PSP.cpp
@@ -21,6 +21,23 @@ bool Video::setupGL() {
         return false;
     }
 
+    SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 5 );
+    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 5 );
+    SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 5 );
+
+    SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 0);
+    SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 0);
+    SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE, 0);
+
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_RED_SIZE, 0);
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_GREEN_SIZE, 0);
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_BLUE_SIZE, 0);
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_ALPHA_SIZE, 0);
+
+    SDL_GL_SetAttribute( SDL_GL_ACCELERATED_VISUAL, 1);
+
+    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+
     //  Create a window
     auto surface = SDL_SetVideoMode(getWindowWidth(),
                                     getWindowHeight(),
@@ -44,8 +61,29 @@ bool Video::setupGL() {
     DebugGlCall(glEnable(GL_TEXTURE_2D));
     DebugGlCall(glShadeModel(GL_SMOOTH));
     DebugGlCall(glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE));
-    DebugGlCall(glEnable(GL_BLEND));
-    DebugGlCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+    DebugGlCall(glEnableClientState(GL_VERTEX_ARRAY)); // For glVertexPointer
+    DebugGlCall(glEnableClientState(GL_TEXTURE_COORD_ARRAY)); // For glTexCoordPointer
+
+    // Disable, as it is not used and may affect performance:
+    DebugGlCall(glDisableClientState(GL_COLOR_ARRAY));
+    DebugGlCall(glDisableClientState(GL_NORMAL_ARRAY));
+
+    DebugGlCall(glDisable(GL_FOG));
+    DebugGlCall(glDisable(GL_LIGHTING));
+    DebugGlCall(glDisable(GL_CULL_FACE));
+    DebugGlCall(glDisable(GL_ALPHA_TEST));
+    DebugGlCall(glDisable(GL_BLEND));
+    DebugGlCall(glDisable(GL_COLOR_LOGIC_OP));
+    DebugGlCall(glDisable(GL_DITHER));
+    DebugGlCall(glDisable(GL_STENCIL_TEST));
+    DebugGlCall(glDisable(GL_DEPTH_TEST));
+    DebugGlCall(glDisable(GL_POINT_SMOOTH));
+    DebugGlCall(glDisable(GL_LINE_SMOOTH));
+    DebugGlCall(glDisable(GL_SCISSOR_TEST));
+    DebugGlCall(glDisable(GL_COLOR_MATERIAL));
+    DebugGlCall(glDisable(GL_NORMALIZE));
+    DebugGlCall(glDisable(GL_RESCALE_NORMAL));
 
     log_info("Exiting Video::setupGL, success.");
     return true;
