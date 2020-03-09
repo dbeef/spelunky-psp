e5d2e52d9eb54e9e7a83752bec258b57d35c5f48
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-03-09 20:20:41 +0100

    Using GL_RGB as GL_RGB8 is not found in OpenGL ES 1.0

--- src/graphics_utils/src/CreateTexture.cpp
+++ src/graphics_utils/src/CreateTexture.cpp
@@ -99,7 +99,7 @@ GLuint graphics_utils::createTexture(const char *data, std::size_t size)
              tex.width, roundedWidth, tex.height, roundedHeight);
     log_info("Uploading texture.");
 
-    DebugGlCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, roundedWidth, roundedHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, tex.buffer));
+    DebugGlCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, rounded_width, rounded_height, 0, GL_RGB, GL_UNSIGNED_BYTE, tex.buffer));
 
     // Don't need the buffer anymore:
     dispose_buffer(tex);
