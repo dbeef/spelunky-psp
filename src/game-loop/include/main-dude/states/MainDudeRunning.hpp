61e35bc92709659ecb7f40f96ee72c5cc8d612a7
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-05-12 22:52:02 +0200

    QuadComponent

--- src/game-loop/include/main-dude/states/MainDudeRunning.hpp
+++ src/game-loop/include/main-dude/states/MainDudeRunning.hpp
@@ -6,7 +6,6 @@ class MainDudeRunning : public MainDudeBaseState
 {
 public:
 
-    MainDudeRunning();
     MainDudeBaseState* update(MainDude&, uint32_t delta_time_ms) override;
     void enter(MainDude&) override;
 };
---
+++ src/game-loop/src/components/InputComponent.cpp
@@ -0,0 +1,27 @@
+#include "components/InputComponent.hpp"
+#include "Input.hpp"
+#include "main-dude/MainDude.hpp"
+
+void InputComponent::update(MainDude& main_dude, uint32_t delta_time_ms)
+{
+    // TODO: Input polling timer
+    Input& input = Input::instance();
+
+    if (input.square())
+    {
+        main_dude._physics_component.add_velocity(-0.025f, 0.0f);
+    }
+    if (input.circle())
+    {
+        main_dude._physics_component.add_velocity(+0.025f, 0.0f);
+    }
+    if (input.triangle())
+    {
+        if (main_dude._physics_component.is_bottom_collision())
+        {
+            main_dude._physics_component.add_velocity(0.0f, -0.18f);
+        }
+    }
+
+}
+
