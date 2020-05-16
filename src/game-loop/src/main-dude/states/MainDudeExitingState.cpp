f2d02f0ef555b5e842ef6dd91502afc10bc16ef5
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-05-16 16:36:40 +0200

    Follow-up

--- src/game-loop/src/main-dude/states/MainDudeExitingState.cpp
+++ src/game-loop/src/main-dude/states/MainDudeExitingState.cpp
@@ -2,17 +2,17 @@
 #include "main-dude/MainDude.hpp"
 #include "Input.hpp"
 
-void MainDudeDucking::enter(MainDude& main_dude)
+void MainDudeExitingState::enter(MainDude& main_dude)
 {
 }
 
-MainDudeBaseState* MainDudeDucking::update(MainDude& main_dude, uint32_t delta_time_ms)
+MainDudeBaseState* MainDudeExitingState::update(MainDude& main_dude, uint32_t delta_time_ms)
 {
     // TODO
     return this;
 }
 
-MainDudeBaseState *MainDudeDucking::handle_input(MainDude& main_dude, const Input &input)
+MainDudeBaseState *MainDudeExitingState::handle_input(MainDude& main_dude, const Input &input)
 {
     return this;
 }
