88449e09734df51959da74383912afa03ce7df39
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-05-16 10:57:11 +0200

    Added stubs for main dude falling/jumping states

---
+++ src/game-loop/src/main-dude/states/MainDudeJumpingState.cpp
@@ -0,0 +1,12 @@
+#include "main-dude/states/MainDudeJumpingState.hpp"
+#include "main-dude/MainDude.hpp"
+
+void MainDudeJumping::enter(MainDude& main_dude)
+{
+}
+
+MainDudeBaseState* MainDudeJumping::update(MainDude& main_dude, uint32_t delta_time_ms)
+{
+    // TODO
+    return this;
+}
