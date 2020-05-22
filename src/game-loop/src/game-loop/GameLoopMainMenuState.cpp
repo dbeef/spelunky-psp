eb843557bc4e2cbd1d1fadd3fb323c8372cd1d06
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-05-22 16:56:42 +0200

    Initializing camera and main dude on fixed position in main menu screen

--- src/game-loop/src/game-loop/GameLoopMainMenuState.cpp
+++ src/game-loop/src/game-loop/GameLoopMainMenuState.cpp
@@ -9,6 +9,7 @@
 
 namespace
 {
+    // TODO: Remove once camera following main-dude is implemented.
     void handle_input()
     {
         const auto& input = Input::instance();
@@ -78,8 +79,12 @@ void GameLoopMainMenuState::enter(GameLoop& game_loop)
     LevelGenerator::instance().getLevel().generate_cave_background();
     LevelGenerator::instance().getLevel().batch_vertices();
     LevelGenerator::instance().getLevel().add_render_entity();
-    
-    game_loop._game_objects.emplace_back(std::make_shared<MainDude>());
+
+    auto &camera = Camera::instance();
+    camera.setX(5.0f);
+    camera.setY(7.2f);
+
+    game_loop._game_objects.emplace_back(std::make_shared<MainDude>(17.37f, 18.5f));
 }
 
 void GameLoopMainMenuState::exit(GameLoop &)
