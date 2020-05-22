88749d04584a519e2c591b301d9331256f97d17f
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-05-22 16:29:29 +0200

    Added game-loop-state stubs

---
+++ src/game-loop/interface/GameLoopMainMenuState.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "GameLoopBaseState.hpp"
+#include <cstdint>
+
+class GameLoop;
+
+class GameLoopMainMenuState : public GameLoopBaseState
+{
+public:
+    GameLoopBaseState* update(GameLoop&, uint32_t delta_time_ms) override;
+    void enter(GameLoop&) override;
+    void exit(GameLoop&) override;
+};
+
---
+++ src/game-loop/src/game-loop/GameLoopMainMenuState.cpp
@@ -0,0 +1,16 @@
+#include "GameLoopMainMenuState.hpp"
+
+GameLoopBaseState *GameLoopMainMenuState::update(GameLoop &, uint32_t delta_time_ms)
+{
+    return this;
+}
+
+void GameLoopMainMenuState::enter(GameLoop &)
+{
+
+}
+
+void GameLoopMainMenuState::exit(GameLoop &)
+{
+
+}
