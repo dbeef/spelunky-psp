30c73b813dfba4c45808f2a75e32c24cc904c5eb
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-06-04 08:58:47 +0200

    Added currently held item overlay to HUD

--- src/game-loop/include/game-objects/HUD.hpp
+++ src/game-loop/include/game-objects/HUD.hpp
@@ -14,11 +14,13 @@ public:
             , _dollar_quad(TextureType::HUD, Renderer::EntityType::SCREEN_SPACE, ICON_WIDTH_PIXELS, ICON_HEIGHT_PIXELS)
             , _ropes_quad(TextureType::HUD, Renderer::EntityType::SCREEN_SPACE, ICON_WIDTH_PIXELS, ICON_HEIGHT_PIXELS)
             , _bombs_quad(TextureType::HUD, Renderer::EntityType::SCREEN_SPACE, ICON_WIDTH_PIXELS, ICON_HEIGHT_PIXELS)
+            , _hold_item_quad(TextureType::HUD, Renderer::EntityType::SCREEN_SPACE, ICON_WIDTH_PIXELS, ICON_HEIGHT_PIXELS)
     {
         _heart_quad.frame_changed<HUDSpritesheetFrames>(HUDSpritesheetFrames::HEART);
         _dollar_quad.frame_changed<HUDSpritesheetFrames>(HUDSpritesheetFrames::DOLLAR_SIGN);
         _ropes_quad.frame_changed<HUDSpritesheetFrames>(HUDSpritesheetFrames::ROPE_ICON);
         _bombs_quad.frame_changed<HUDSpritesheetFrames>(HUDSpritesheetFrames::BOMB_ICON);
+        _hold_item_quad.frame_changed<HUDSpritesheetFrames>(HUDSpritesheetFrames::HOLD_ITEM_ICON);
 
         const float icons_offset_pixels = Video::instance().get_window_width() * 0.1f;
 
@@ -26,11 +28,13 @@ public:
         _dollar_center.x = pos_x + (icons_offset_pixels * 1);
         _ropes_center.x = pos_x + (icons_offset_pixels * 2);
         _bombs_center.x = pos_x + (icons_offset_pixels * 3);
+        _hold_item_center.x = pos_x + (icons_offset_pixels * 4);
 
         _heart_center.y = pos_y;
         _dollar_center.y = pos_y;
         _ropes_center.y = pos_y;
         _bombs_center.y = pos_y;
+        _hold_item_center.y = pos_y;
     }
 
     void update(uint32_t delta_time_ms) override
@@ -39,6 +43,7 @@ public:
         _dollar_quad.update(_dollar_center.x, _dollar_center.y);
         _ropes_quad.update(_ropes_center.x, _ropes_center.y);
         _bombs_quad.update(_bombs_center.x, _bombs_center.y);
+        _hold_item_quad.update(_bombs_center.x, _bombs_center.y);
     }
 
 private:
@@ -50,9 +55,11 @@ private:
     Point2D _dollar_center;
     Point2D _ropes_center;
     Point2D _bombs_center;
+    Point2D _hold_item_center;
 
     QuadComponent _heart_quad;
     QuadComponent _dollar_quad;
     QuadComponent _ropes_quad;
     QuadComponent _bombs_quad;
+    QuadComponent _hold_item_quad;
 };
