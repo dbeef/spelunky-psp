cb67ba428cf66dd95ab0987b27fc0aded98879c9
Rafal Bedzkowski <ravcik@gmail.com>
2020-06-16 22:11:11 +0200

    Timestep::delay() returns min. delay

--- src/time/src/Timestep.cpp
+++ src/time/src/Timestep.cpp
@@ -4,16 +4,11 @@ Timestamp Timestep::delay() const
 {
     const auto delta_ms = (_end - _start);
 
-    if (delta_ms > _frequency)
+    if (delta_ms <= _frequency)
     {
-        // no delay
-        return delta_ms;
-    }
-    else
-    {
-        auto sleep_time_ms = _frequency - delta_ms;
+        const auto sleep_time_ms = _frequency - delta_ms;
         SDL_Delay(sleep_time_ms);
-        return sleep_time_ms;
+        return _frequency;
     }
     return delta_ms;
 }
