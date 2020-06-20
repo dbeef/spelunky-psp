077c60ba466b3b9682558e94a6ac3f2e929f0c10
Rafał Będźkowski <ravcik@gmail.com>
2020-06-21 00:42:46 +0200

    Update Windows SDL2 downloaded artifact

--- scripts/dependencies-install.ps1
+++ scripts/dependencies-install.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param (
     [Parameter()][String]
-    $Version="1.2.15",
+    $Version="2.0.12",
     
     [Parameter()][String]
     $DepsDirectory="deps"
@@ -11,13 +11,13 @@ $ErrorActionPreference = "Stop"
 
 Try {
     Try {
-        Invoke-WebRequest -Uri "https://www.libsdl.org/release/SDL-devel-$Version-VC.zip" -OutFile "SDL.zip"
+        Invoke-WebRequest -Uri "https://www.libsdl.org/release/SDL2-devel-$Version-VC.zip" -OutFile "SDL.zip"
     }
     Catch {
         Invoke-WebRequest -Uri "https://osdn.net/frs/g_redir.php?m=kent&f=libsdl%2FSDL%2F$Version%2FSDL-devel-$Version-VC.zip" -OutFile "SDL.zip"
     }
     If (-Not (Test-Path "SDL.zip")) {
-        Write-Error "Could not download SDL dependency"
+        Write-Error "Could not download SDL2 dependency"
         Exit 1
     }
     
@@ -28,9 +28,8 @@ Try {
 
     [System.IO.Compression.ZipFile]::ExtractToDirectory("SDL.zip", "$DepsDirectory\")
     
-    New-Item -Path "$DepsDirectory\SDL\include\SDL" -ItemType "Directory"
-    Move-Item -Path "$DepsDirectory\SDL-$Version\lib" -Destination "$DepsDirectory\SDL\" -Force
-    Move-Item -Path "$DepsDirectory\SDL-$Version\include\*.h" -Destination "$DepsDirectory\SDL\include\SDL\" -Force
+    Move-Item -Path "$DepsDirectory\SDL2-$Version\lib" -Destination "$DepsDirectory\SDL2\" -Force
+    Move-Item -Path "$DepsDirectory\SDL2-$Version\include\*.h" -Destination "$DepsDirectory\SDL2\include\" -Force
     Remove-Item -Path "$DepsDirectory\SDL-$Version" -Force -Recurse
 }
 Catch {
