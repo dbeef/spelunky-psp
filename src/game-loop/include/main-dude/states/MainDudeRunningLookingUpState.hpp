40c03c1b767ac9eabc43720a68b2aa5e8571ba07
Rafal Bedzkowski <ravcik@gmail.com>
2020-06-25 23:55:50 +0200

    Main dude can look up when running

---
+++ src/game-loop/include/main-dude/states/MainDudeRunningLookingUpState.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "main-dude/states/MainDudeBaseState.hpp"
+
+class MainDudeRunningLookingUpState : public MainDudeBaseState
+{
+public:
+
+    MainDudeBaseState* update(MainDude&, uint32_t delta_time_ms) override;
+    MainDudeBaseState* handle_input(MainDude&, const Input& input) override;
+    void enter(MainDude&) override;
+};
