bb925e331b757529d8291a619ab0af627f2f1aed
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-08-04 16:33:45 +0200

    Added a note to myself

--- src/game-loop/interface/GameLoopPlayingState.hpp
+++ src/game-loop/interface/GameLoopPlayingState.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <memory>
+#include <cstdint>
+
 #include "GameLoopBaseState.hpp"
 
 class GameLoop;
@@ -14,7 +16,34 @@ public:
     void enter(GameLoop&) override;
     void exit(GameLoop&) override;
 private:
+
+    // Or should HUD store this?
+    struct
+    {
+        std::uint16_t hearts = 4;
+        std::uint16_t bombs = 4;
+        std::uint16_t ropes = 4;
+        std::uint16_t gold = 0;
+    } _status;
+
+    // Possible places to store status:
+    //
+    // - GameLoopPlayingState
+    // - HUD
+    // - MainDude
+    //
+    // MainDude would be easiest, yet HUD still needs to be informed about the amounts.
+    // Could pass HUD sptr to main dude.
+    //
+    // If placed in the GameLoopPlayingState, GameLoopPlayingState would be the middle man having
+    // pointers to both sides. HUD could store its copy of MainDude's state amounts, and game loop playing state would
+    // update them.
+    //
+    // Or, MainDude could store the information and HUD could get a pointer to the main dude instance, updating
+    // amounts when they change.
+    //
+    // Or... Event system? Subscription on main dude's gold/hearts/ropes/bombes count changed?
+
     std::shared_ptr<PauseOverlay> _pause_overlay = nullptr;
     std::shared_ptr<DeathOverlay> _death_overlay = nullptr;
 };
-
