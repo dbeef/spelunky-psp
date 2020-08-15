57875987e5e1a860879c1376f7bb27f2eb6b43ad
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-08-15 13:44:14 +0200

    Implemented observer pattern.

---
+++ src/patterns/interface/patterns/Subject.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Observer.hpp"
+
+#include <vector>
+#include <algorithm>
+
+template<class EventType>
+class Subject
+{
+public:
+
+    Subject()
+    {
+        const std::size_t DEFAULT_OBSERVERS_CAPACITY = 8;
+        _observers.reserve(DEFAULT_OBSERVERS_CAPACITY);
+    }
+
+    ~Subject() = default;
+
+    void add_observer(Observer<EventType>* observer)
+    {
+        _observers.emplace_back(observer);
+    }
+
+    void remove_observer(const Observer<EventType>* observer)
+    {
+        std::remove_if(_observers.begin(), _observers.end(), [observer](const auto& other){ return observer == other; });
+    }
+
+    void notify(EventType event)
+    {
+        for (auto& observer : _observers)
+        {
+            observer->on_notify(event);
+        }
+    }
+
+private:
+
+    std::vector<Observer<EventType>*> _observers;
+};
