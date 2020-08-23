076b5535d7bfa884d5dfc24e7ff21ea47d77cbba
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-08-23 13:05:24 +0200

    Random chance of spawning a gold bar / limited gold bars

--- src/game-loop/src/populator/LootPopulator.cpp
+++ src/game-loop/src/populator/LootPopulator.cpp
@@ -5,10 +5,25 @@
 #include "logger/log.h"
 
 #include <utility>
+#include <random>
+
+namespace
+{
+    int get_random_number()
+    {
+        static std::random_device random_device;
+        std::default_random_engine engine(random_device());
+        std::uniform_int_distribution<int> uniform_dist(0, 10);
+        return uniform_dist(engine);
+    }
+
+    const std::uint16_t MAX_GOLD_BARS_IN_LEVEL = 10;
+}
 
 std::vector<std::shared_ptr<GameEntity>> populator::generate_loot()
 {
     std::vector<std::shared_ptr<GameEntity>> out{};
+    std::uint16_t gold_bars_spawned = 0;
 
     const auto& tile_batch = Level::instance().get_tile_batch();
 
@@ -17,9 +32,16 @@ std::vector<std::shared_ptr<GameEntity>> populator::generate_loot()
         for (int tile_y = 0; tile_y < Consts::LEVEL_HEIGHT_TILES; tile_y++)
         {
             const auto loot_type = tile_batch.get_loot_type_spawned_at(tile_x, tile_y);
-            if (loot_type == LootType::GOLD_BAR)
+            bool spawned = (get_random_number() % 5) == 0;
+
+            if (spawned)
             {
-                out.push_back(std::make_shared<GoldBar>(tile_x + (MapTile::PHYSICAL_WIDTH / 2.0f), tile_y + (MapTile::PHYSICAL_HEIGHT / 2.0f)));
+                if (loot_type == LootType::GOLD_BAR && gold_bars_spawned < MAX_GOLD_BARS_IN_LEVEL)
+                {
+                    out.push_back(std::make_shared<GoldBar>(tile_x + (MapTile::PHYSICAL_WIDTH / 2.0f),
+                                                            tile_y + (MapTile::PHYSICAL_HEIGHT / 2.0f)));
+                    gold_bars_spawned++;
+                }
             }
         }
     }
