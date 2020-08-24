4853d26ed42490dc25b8511cbfafd4032b1b4b14
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-08-24 10:26:08 +0200

    GameLoopPlayingState as MainDudeEvent's observer

--- src/game-loop/interface/GameLoopPlayingState.hpp
+++ src/game-loop/interface/GameLoopPlayingState.hpp
@@ -1,18 +1,22 @@
 #pragma once
 
 #include <memory>
+
 #include "GameLoopBaseState.hpp"
+#include "patterns/Observer.hpp"
 
 class GameLoop;
 class PauseOverlay;
 class DeathOverlay;
+enum class MainDudeEvent;
 
-class GameLoopPlayingState : public GameLoopBaseState
+class GameLoopPlayingState : public GameLoopBaseState, public Observer<MainDudeEvent>
 {
 public:
     GameLoopBaseState* update(GameLoop&, uint32_t delta_time_ms) override;
     void enter(GameLoop&) override;
     void exit(GameLoop&) override;
+    void on_notify(const MainDudeEvent*) override;
 private:
     std::shared_ptr<PauseOverlay> _pause_overlay = nullptr;
     std::shared_ptr<DeathOverlay> _death_overlay = nullptr;
