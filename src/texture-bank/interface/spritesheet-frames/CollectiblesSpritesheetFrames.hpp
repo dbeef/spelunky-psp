5293cc2bf2c08b089a32d323deb8099de0c74dbb
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-09-20 14:52:31 +0200

    Added new collectibles spritesheet (jar, rock, chest, gems)

--- src/texture-bank/interface/spritesheet-frames/CollectiblesSpritesheetFrames.hpp
+++ src/texture-bank/interface/spritesheet-frames/CollectiblesSpritesheetFrames.hpp
@@ -5,5 +5,11 @@
 enum class CollectiblesSpritesheetFrames : std::uint16_t
 {
     GOLD_BAR = 0,
-    TRIPLE_GOLD_BAR
+    TRIPLE_GOLD_BAR,
+    DIAMOND_BIG,
+    EMERALD_BIG,
+    RUBY_BIG,
+    CHEST,
+    JAR,
+    ROCK
 };
