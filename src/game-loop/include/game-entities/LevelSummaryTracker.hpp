d52a8a0c3993b0c4c760d5dc9c5131c203ca0234
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-09-20 18:51:44 +0200

    Keeping track of dollar count

--- src/game-loop/include/game-entities/LevelSummaryTracker.hpp
+++ src/game-loop/include/game-entities/LevelSummaryTracker.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
 #include "game-entities/GameEntity.hpp"
+#include "other/InventoryEvent.hpp"
+#include "patterns/Observer.hpp"
+
 #include <cstdint>
 
-class LevelSummaryTracker : public GameEntity
+class LevelSummaryTracker : public GameEntity, public Observer<InventoryEvent>
 {
 public:
+
+    LevelSummaryTracker();
+    ~LevelSummaryTracker() override;
+
     void update(uint32_t delta_time_ms) override;
+    void on_notify(const InventoryEvent*) override;
 
     void entered_new_level();
     void reset();
@@ -15,8 +23,14 @@ public:
     uint32_t get_time_playing_total_ms() const { return _time_playing_total_ms; }
     uint32_t get_time_playing_current_level_ms() const { return _time_playing_current_level_ms; }
 
+    uint32_t get_dollars_beginning() const { return _dollars_beginning; }
+    uint32_t get_dollars_end() const { return _dollars_end; }
+
 private:
     uint32_t _level_counter = 0;
     uint32_t _time_playing_total_ms = 0;
     uint32_t _time_playing_current_level_ms = 0;
+
+    uint32_t _dollars_beginning = 0;
+    uint32_t _dollars_end = 0;
 };
--- src/game-loop/src/game-entities/LevelSummaryOverlay.cpp
+++ src/game-loop/src/game-entities/LevelSummaryOverlay.cpp
@@ -1,5 +1,4 @@
 #include <string>
-#include <cstring>
 #include <sstream>
 #include <iomanip>
 
@@ -31,25 +30,32 @@ namespace
         return out.str();
     }
 
+    std::string get_money_msg(uint32_t money_beginning, uint32_t money_end)
+    {
+        std::stringstream out;
+        out << "MONEY: " << "$" << money_beginning << " / " << "$" << money_end;
+        return out.str();
+    }
+
     const char* TIME_MSG = "TIME:";
     const char* LOOT_MSG = "LOOT:";
     const char* KILLS_MSG = "KILLS:";
     const char* MONEY_MSG = "MONEY:";
 }
 
-LevelSummaryOverlay::LevelSummaryOverlay(std::shared_ptr<Viewport> viewport, const std::shared_ptr<LevelSummaryTracker>& stats)
+LevelSummaryOverlay::LevelSummaryOverlay(std::shared_ptr<Viewport> viewport, const std::shared_ptr<LevelSummaryTracker>& tracker)
     : _viewport(std::move(viewport))
 {
     {
         const Point2D pos = {_viewport->get_width_world_units() * 0.16f, _viewport->get_height_world_units() * 0.155f};
         _texts.level_completed.set_yellow(true);
-        _texts.level_completed.set_text(get_level_completed_msg(stats->get_current_level()));
+        _texts.level_completed.set_text(get_level_completed_msg(tracker->get_current_level()));
         _texts.level_completed.set_position(pos);
     }
 
     {
         const Point2D pos = {_viewport->get_width_world_units() * 0.16f, _viewport->get_height_world_units() * 0.23f};
-        _texts.time.set_text(get_time_msg(stats->get_time_playing_current_level_ms(), stats->get_time_playing_total_ms()));
+        _texts.time.set_text(get_time_msg(tracker->get_time_playing_current_level_ms(), tracker->get_time_playing_total_ms()));
         _texts.time.set_position(pos);
     }
 
@@ -67,7 +73,7 @@ LevelSummaryOverlay::LevelSummaryOverlay(std::shared_ptr<Viewport> viewport, con
 
     {
         const Point2D pos = {_viewport->get_width_world_units() * 0.16f, _viewport->get_height_world_units() * 0.455f};
-        _texts.money.set_text(MONEY_MSG);
+        _texts.money.set_text(get_money_msg(tracker->get_dollars_beginning(), tracker->get_dollars_end()));
         _texts.money.set_position(pos);
     }
 }
