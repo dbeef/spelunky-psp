24e3f6cf00ccd4316be8adc13b9dbcaacc6b4a5a
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-09-20 18:51:44 +0200

    Keeping track of dollar count

--- src/game-loop/src/game-entities/LevelSummaryTracker.cpp
+++ src/game-loop/src/game-entities/LevelSummaryTracker.cpp
@@ -1,4 +1,5 @@
 #include "game-entities/LevelSummaryTracker.hpp"
+#include "other/Inventory.hpp"
 
 void LevelSummaryTracker::update(uint32_t delta_time_ms)
 {
@@ -10,6 +11,7 @@ void LevelSummaryTracker::entered_new_level()
 {
     _level_counter += 1;
     _time_playing_current_level_ms = 0;
+    _dollars_beginning = _dollars_end;
 }
 
 void LevelSummaryTracker::reset()
@@ -17,4 +19,24 @@ void LevelSummaryTracker::reset()
     _level_counter = 0;
     _time_playing_total_ms = 0;
     _time_playing_current_level_ms = 0;
+    _dollars_beginning = 0;
+    _dollars_end = 0;
+}
+
+void LevelSummaryTracker::on_notify(const InventoryEvent* event)
+{
+    if (*event == InventoryEvent::DOLLARS_COUNT_CHANGED)
+    {
+        _dollars_end = Inventory::instance().get_dollars();
+    }
+}
+
+LevelSummaryTracker::LevelSummaryTracker()
+{
+    Inventory::instance().add_observer(this);
+}
+
+LevelSummaryTracker::~LevelSummaryTracker()
+{
+    Inventory::instance().remove_observer(this);
 }
