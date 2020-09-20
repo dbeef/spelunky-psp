fca2a4d695f871ee2b50c2180fef7c1237bd571c
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-09-20 16:01:50 +0200

    Playing coin sounds on collected

--- src/game-loop/src/game-entities/BigGem.cpp
+++ src/game-loop/src/game-entities/BigGem.cpp
@@ -3,6 +3,7 @@
 #include "spritesheet-frames/CollectiblesSpritesheetFrames.hpp"
 #include "other/PhysicsComponentAggregator.hpp"
 #include "other/Inventory.hpp"
+#include "audio/Audio.hpp"
 
 namespace
 {
@@ -32,6 +33,7 @@ BigGem::BigGem(float x_center, float y_center)
         if (other->get_type() == PhysicsComponentType::MAIN_DUDE)
         {
             Inventory::instance().add_dollars(1000);
+            Audio::instance().play(SFXType::COIN);
             mark_for_disposal();
         }
     });
