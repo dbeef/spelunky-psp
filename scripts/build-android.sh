047ebf77deccb54cc6f8b5d3796c82ea8fc0ab6b
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-09-26 16:21:23 +0200

    Updated Android build scripts

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -3,4 +3,16 @@
 
 source scripts/variables-android.sh
 
-cmake --build $BUILD_PATH --target install --config Release -j `nproc`
+# Following variables must be present in the environment:
+# ANDROID_NDK_HOME - Root of directory with unpacked Android NDK, i.e - /opt/android/android-ndk-r15c
+# ANDROID_DEPS_ARMEABI_V7A - Root of directory with armeabi-v7a dependency artifacts, i.e - /opt/android/deps-armeabi-v7a,
+#                            will be forwarded to CMake through CMAKE_PREFIX_PATH.
+# ANDROID_DEPS_ARM64_V8A - Root of directory with arm64-v8a dependency artifacts, i.e - /opt/android/deps-arm64-v8a,
+#                            will be forwarded to CMake through CMAKE_PREFIX_PATH.
+
+rm -rf $INSTALL_PATH
+mkdir -p $INSTALL_PATH
+
+cd $ROOT_PATH/platforms/android
+./gradlew assemble
+cp build/android-app/outputs/apk/debug/Spelunky_PSP-debug.apk $INSTALL_PATH/Spelunky_PSP.apk
