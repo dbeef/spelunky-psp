b459848f3e5e8ce0921ae869ca57404993e27231
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2021-01-31 19:18:24 +0100

    Added NPC spritesheet with bat and snake frames.

---
+++ src/texture-bank/interface/spritesheet-frames/NPCSpritesheetFrames.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstdint>
+
+enum class NPCSpritesheetFrames : std::uint16_t
+{
+    BAT_HANG = 0,
+    BAT_FLYING_0_FIRST,
+    BAT_FLYING_1,
+    BAT_FLYING_2_LAST,
+    SNAKE,
+    SNAKE_WALK_0_FIRST,
+    SNAKE_WALK_1,
+    SNAKE_WALK_2,
+    SNAKE_WALK_3_LAST,
+};
