cf108cd95a19d60f803145a2e64b1fd9f669de92
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2021-02-06 16:46:58 +0100

    Implemented ArrowTrap

--- src/game-loop/src/prefabs/traps/ArrowTrap.cpp
+++ src/game-loop/src/prefabs/traps/ArrowTrap.cpp
@@ -1,16 +1,21 @@
-#include <components/generic/QuadComponent.hpp>
-#include <spritesheet-frames/NPCSpritesheetFrames.hpp>
-#include <components/generic/MeshComponent.hpp>
 #include "prefabs/traps/ArrowTrap.hpp"
+#include "prefabs/items/Arrow.hpp"
 
 #include "components/generic/HorizontalOrientationComponent.hpp"
 #include "components/generic/PositionComponent.hpp"
 #include "components/generic/ScriptingComponent.hpp"
+#include "components/specialized/MainDudeComponent.hpp"
 
+#include "spritesheet-frames/NPCSpritesheetFrames.hpp"
 #include "EntityRegistry.hpp"
+
+#include <cmath>
 
 namespace
 {
+    constexpr float activation_distance_x = 7;
+    constexpr float activation_distance_y = 0.5f;
+
     class ArrowTrapScript : public ScriptBase
     {
     public:
@@ -19,6 +24,56 @@ namespace
         void update(entt::entity owner, uint32_t delta_time_ms) override
         {
             auto& registry = EntityRegistry::instance().get_registry();
+
+            const auto& arrow_trap_position = registry.get<PositionComponent>(owner);
+            const auto& arrow_trap_physics = registry.get<PhysicsComponent>(owner);
+            const auto bodies = registry.view<PhysicsComponent, PositionComponent>();
+            // TODO: Trap triggering should also take into consideration map tiles that
+            //       may obscure bodies.
+
+            bool triggered = false;
+
+            bodies.each([&](PhysicsComponent& other_physics, PositionComponent& other_position)
+            {
+                if (triggered)
+                {
+                    return;
+                }
+
+                if (other_physics.get_y_velocity() == 0.0f && other_physics.get_x_velocity() == 0.0f)
+                {
+                    return;
+                }
+
+                float distance_x = std::fabs(other_position.x_center - arrow_trap_position.x_center);
+                float distance_y = std::fabs(other_position.y_center - arrow_trap_position.y_center);
+
+                if (distance_x < activation_distance_x && distance_y < activation_distance_y)
+                {
+                    triggered = true;
+
+                    auto arrow = prefabs::Arrow::create(arrow_trap_position.x_center, arrow_trap_position.y_center);
+                    auto& arrow_physics = registry.get<PhysicsComponent>(arrow);
+                    auto& arrow_position = registry.get<PositionComponent>(arrow);
+                    switch (_orientation)
+                    {
+                        case HorizontalOrientation::LEFT:
+                        {
+                            arrow_physics.set_x_velocity(-0.25f);
+                            arrow_position.x_center -= 0.75f;
+                            break;
+                        }
+                        case HorizontalOrientation::RIGHT:
+                        {
+                            arrow_physics.set_x_velocity(+0.25f);
+                            arrow_position.x_center += 0.75f;
+                            break;
+                        }
+                    }
+
+                    registry.destroy(owner);
+                }
+            });
         }
 
     private:
@@ -32,19 +87,12 @@ entt::entity prefabs::ArrowTrap::create(float pos_x_center, float pos_y_center, 
 
     const auto entity = registry.create();
 
+    PhysicsComponent physics(1.0f, 1.0f);
+    physics.disable_gravity();
+
     registry.emplace<PositionComponent>(entity, pos_x_center, pos_y_center);
+    registry.emplace<PhysicsComponent>(entity, physics);
     registry.emplace<ScriptingComponent>(entity, std::make_shared<ArrowTrapScript>(orientation));
-
-    // Temporarily:
-
-    QuadComponent quad(TextureType::NPC, 1.0f, 1.0f);
-    MeshComponent mesh;
-    quad.frame_changed(NPCSpritesheetFrames::SPIDER_HANG);
-    mesh.rendering_layer = RenderingLayer::LAYER_2_ITEMS;
-    mesh.camera_type = CameraType::MODEL_VIEW_SPACE;
-
-    registry.emplace<QuadComponent>(entity, quad);
-    registry.emplace<MeshComponent>(entity, mesh);
 
     return entity;
 }
