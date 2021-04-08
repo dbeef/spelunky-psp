5997e4d57cc45287dbf4f581d79d1f4b1c96a03b
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2021-04-08 13:29:49 +0200

    Increased cooldown

--- src/game-loop/include/components/damage/GiveNpcTouchDamageComponent.hpp
+++ src/game-loop/include/components/damage/GiveNpcTouchDamageComponent.hpp
@@ -6,6 +6,6 @@
 using NpcDamage_t = int;
 struct GiveNpcTouchDamageComponent : public Subject<NpcDamage_t>
 {
-    static const int TOP_COOLDOWN_MS = 750;
+    static const int TOP_COOLDOWN_MS = 1000;
     int cooldown_ms = TOP_COOLDOWN_MS;
 };
