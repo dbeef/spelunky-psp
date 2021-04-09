d43e46f9ac720f890ae8968ca51cdba3267734e0
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2021-04-09 12:38:25 +0200

    Cleanup

--- src/game-loop/include/components/damage/GiveSpikesDamageComponent.hpp
+++ src/game-loop/include/components/damage/GiveSpikesDamageComponent.hpp
@@ -2,9 +2,6 @@
 
 struct GiveSpikesDamageComponent
 {
-    // Spikes cause instant death - no damage parameter
-    // TODO: Treshold speed?
-
     // https://github.com/skypjack/entt/issues/330
     char dummy_member_for_entt;
 };
