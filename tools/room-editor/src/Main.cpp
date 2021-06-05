9dfba2a073f35abdac8c96fd5a4c6b544e296007
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2021-06-05 08:46:41 +0200

    Some structural changes

--- tools/room-editor/src/Main.cpp
+++ tools/room-editor/src/Main.cpp
@@ -1,14 +1,3 @@
-/*******************************************************************************************
-*
-*   raylib [textures] example - sprite button
-*
-*   This example has been created using raylib 2.5 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2019 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include <cstdlib>
 #include <string>
 #include <iostream>
@@ -20,52 +9,30 @@
 namespace
 {
     Texture2D tiles[static_cast<std::size_t>(MapTileType::_SIZE)];
-}
-
-int main()
-{
-    const std::string assetsPath = "/home/dbeef/Desktop/spelunky-psp/";
-
+    std::size_t selected_tile_index = -1;
+    Camera2D camera = { 0 };
+    Vector2 mouse_point = { 0.0f, 0.0f };
+    const float tile_width = 16;
+    const float tile_height = 16;
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "SpelunkyPSP room editor");
-
-    // Load the tiles:
-
-    for (auto index = static_cast<std::size_t>(MapTileType::NOTHING); index < static_cast<std::size_t>(MapTileType::_SIZE); index++)
+    void load_tiles(const std::string& assetsPath)
     {
-        const std::string path = assetsPath + "/assets/tilesheets/level-tiles/" + std::to_string(index) + ".png";
-        std::cout << "Loading tile from: " << path << std::endl;
+        for (auto index = static_cast<std::size_t>(MapTileType::NOTHING); index < static_cast<std::size_t>(MapTileType::_SIZE); index++)
+        {
+            const std::string path = assetsPath + "/assets/tilesheets/level-tiles/" + std::to_string(index) + ".png";
+            std::cout << "Loading tile from: " << path << std::endl;
 
-        auto& tile = tiles[index];
+            auto& tile = tiles[index];
 
-        tile = LoadTexture(path.c_str());
-        std::cout << "Params: " << tile.width << ", " << tile.height << ", " << tile.id << std::endl;
+            tile = LoadTexture(path.c_str());
+            std::cout << "Params: " << tile.width << ", " << tile.height << ", " << tile.id << std::endl;
+        }
     }
 
-    // Render loop:
-
-
-    Camera2D camera = { 0 };
-    camera.target = {};
-    camera.offset = (Vector2){ 2, 2 };
-    camera.rotation = 0.0f;
-    camera.zoom = 1.6f;
-
-    Vector2 mouse_point = { 0.0f, 0.0f };
-
-    SetTargetFPS(60);
-    while (!WindowShouldClose())
+    void display_menu()
     {
-        mouse_point = GetMousePosition();
-
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        BeginMode2D(camera);
-
-        const float tile_width = 16;
-        const float tile_height = 16;
         Vector2 position {0, 0}; // By convention, it's a left-upper corner of the rectangle
 
         std::size_t index = 0;
@@ -78,10 +45,16 @@ int main()
             if (CheckCollisionPointRec(mouse_point, {camera.zoom * position.x, camera.zoom * position.y,
                                                      camera.zoom * dimensions.width, camera.zoom * dimensions.height}))
             {
-                DrawTextureRec(tile, {0, 0, dimensions.width * 1.1f, dimensions.height * 1.1f}, position, GREEN);
+                selected_tile_index = index;
 
-                if (IsMouseButtonDown(MouseButton::MOUSE_LEFT_BUTTON)) {}
-                if (IsMouseButtonReleased(MouseButton::MOUSE_LEFT_BUTTON)) {}
+                if (IsMouseButtonDown(MouseButton::MOUSE_LEFT_BUTTON))
+                {
+                    DrawTextureRec(tile, {0, 0, dimensions.width * 1.1f, dimensions.height * 1.1f}, position, BLUE);
+                }
+                else
+                {
+                    DrawTextureRec(tile, {0, 0, dimensions.width * 1.1f, dimensions.height * 1.1f}, position, GREEN);
+                }
             }
             else
             {
@@ -100,6 +73,33 @@ int main()
 
             index++;
         }
+    }
+
+    bool has_selected_tile() { return selected_tile_index != -1; }
+    Texture2D& get_selected_tile_texture() { assert(has_selected_tile()); return tiles[selected_tile_index]; }
+}
+
+int main()
+{
+    InitWindow(screenWidth, screenHeight, "SpelunkyPSP room editor");
+
+    load_tiles("/home/dbeef/Desktop/spelunky-psp/"); // FIXME: Maybe pass via cmdline args? With fallback via relative paths?
+
+    camera.target = {};
+    camera.offset = (Vector2){ 2, 2 };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.6f;
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        mouse_point = GetMousePosition();
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        BeginMode2D(camera);
+
+        display_menu();
 
         EndMode2D();
         EndDrawing();
