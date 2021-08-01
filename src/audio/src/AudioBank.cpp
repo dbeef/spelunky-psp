3c6de669d15b64db677f6f63f3c256cbd6fa4c98
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2021-08-01 16:00:51 +0200

    Re-generated assets with modified resource-compiler that outputs .asm instead of .h

--- src/audio/src/AudioBank.cpp
+++ src/audio/src/AudioBank.cpp
@@ -1,211 +1,129 @@
 #include "AudioBank.hpp"
 
-namespace title_wav
-{
-    #include "generated/title.wav.hpp"
-}
-
-namespace cave_wav
-{
-    #include "generated/cave.wav.hpp"
-}
-
-namespace jump_wav
-{
-    #include "generated/jump.wav.hpp"
-}
-
-namespace whip_wav
-{
-    #include "generated/whip.wav.hpp"
-}
-
-namespace entering_door_wav
-{
-    #include "generated/entering_door.wav.hpp"
-}
-
-namespace die_wav
-{
-    #include "generated/die.wav.hpp"
-}
-
-namespace hurt_wav
-{
-    #include "generated/hurt.wav.hpp"
-}
-
-namespace coin_wav
-{
-    #include "generated/coin.wav.hpp"
-}
-
-namespace climb_1_wav
-{
-    #include "generated/climb1.wav.hpp"
-}
-
-namespace climb_2_wav
-{
-    #include "generated/climb2.wav.hpp"
-}
-
-namespace arrow_trap
-{
-    #include "generated/arrowtrap.wav.hpp"
-}
-
-namespace bat
-{
-    #include "generated/bat.wav.hpp"
-}
-
-namespace chest_open
-{
-    #include "generated/chestopen.wav.hpp"
-}
-
-namespace explosion
-{
-    #include "generated/explosion.wav.hpp"
-}
-
-namespace gem
-{
-    #include "generated/gem.wav.hpp"
-}
-
-namespace hit
-{
-    #include "generated/hit.wav.hpp"
-}
-
-namespace jetpack
-{
-    #include "generated/jetpack.wav.hpp"
-}
-
-namespace pickup
-{
-    #include "generated/pickup.wav.hpp"
-}
-
-namespace shotgun
-{
-    #include "generated/shotgun.wav.hpp"
-}
-
-namespace kiss
-{
-    #include "generated/kiss.wav.hpp"
-}
-
-namespace _throw
-{
-    #include "generated/throw.wav.hpp"
-}
+extern const char title_wav[];
+extern const char cave_wav[];
+extern const char jump_wav[];
+extern const char whip_wav[];
+extern const char entering_door_wav[];
+extern const char die_wav[];
+extern const char hurt_wav[];
+extern const char coin_wav[];
+extern const char climb1_wav[];
+extern const char climb2_wav[];
+extern const char arrowtrap_wav[];
+extern const char bat_wav[];
+extern const int chestopen_wav_length;
+extern const char chestopen_wav[];
+extern const char explosion_wav[];
+extern const char gem_wav[];
+extern const char hit_wav[];
+extern const char jetpack_wav[];
+extern const char pickup_wav[];
+extern const char shotgun_wav[];
+extern const char kiss_wav[];
+extern const char throw_wav[];
 
 const char *audio_bank::get_title_theme_wav()
 {
-    return &title_wav::data[0];
+    return title_wav;
 }
 
 const char *audio_bank::get_cave_theme_wav()
 {
-    return &cave_wav::data[0];
+    return cave_wav;
 }
 
 const char *audio_bank::get_jump_wav()
 {
-    return &jump_wav::data[0];
+    return jump_wav;
 }
 
 const char *audio_bank::get_whip_wav()
 {
-    return &whip_wav::data[0];
+    return whip_wav;
 }
 
 const char *audio_bank::get_entering_door_wav()
 {
-    return &entering_door_wav::data[0];
+    return entering_door_wav;
 }
 
 const char *audio_bank::get_die_wav()
 {
-    return &die_wav::data[0];
+    return die_wav;
 }
 
 const char *audio_bank::get_hurt_wav()
 {
-    return &hurt_wav::data[0];
+    return hurt_wav;
 }
 
 const char *audio_bank::get_coin_wav()
 {
-    return &coin_wav::data[0];
+    return coin_wav;
 }
 
 const char *audio_bank::get_climb_1_wav()
 {
-    return &climb_1_wav::data[0];
+    return climb1_wav;
 }
 
 const char *audio_bank::get_climb_2_wav()
 {
-    return &climb_2_wav::data[0];
+    return climb2_wav;
 }
 
 const char *audio_bank::get_arrow_trap_wav()
 {
-    return &arrow_trap::data[0];
+    return arrowtrap_wav;
 }
 
 const char *audio_bank::get_bat_wav()
 {
-    return &bat::data[0];
+    return bat_wav;
 }
 
 const char *audio_bank::get_chest_open_wav()
 {
-    return &chest_open::data[0];
+    return chestopen_wav;
 }
 
 const char *audio_bank::get_explosion_wav()
 {
-    return &explosion::data[0];
+    return explosion_wav;
 }
 
 const char *audio_bank::get_gem_wav()
 {
-    return &gem::data[0];
+    return gem_wav;
 }
 
 const char *audio_bank::get_hit_wav()
 {
-    return &hit::data[0];
+    return hit_wav;
 }
 
 const char *audio_bank::get_jetpack_wav()
 {
-    return &jetpack::data[0];
+    return jetpack_wav;
 }
 
 const char *audio_bank::get_pickup_wav()
 {
-    return &pickup::data[0];
+    return pickup_wav;
 }
 
 const char *audio_bank::get_shotgun_wav()
 {
-    return &shotgun::data[0];
+    return shotgun_wav;
 }
 
 const char *audio_bank::get_throw_wav()
 {
-    return &_throw::data[0];
+    return throw_wav;
 }
 
 const char *audio_bank::get_kiss_wav()
 {
-    return &kiss::data[0];
+    return kiss_wav;
 }
