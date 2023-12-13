c7a8f1f112e3385d66ffeb66601b428545f97416
Daniel Zalega <daniel.zalega@gmail.com>
2023-12-13 22:14:52 +0100

    Desktop build now uses SDL2 instead of SDL1.2 (#145)

--- src/video/src/Video_Desktop.cpp
+++ src/video/src/Video_Desktop.cpp
@@ -1,16 +1,17 @@
-//
-// Created by dbeef on 7/7/19.
-//
 #include "video/Video.hpp"
 #include "graphics_utils/DebugGlCall.hpp"
-#include "glad/glad.h"
+#include "time/Timestep.hpp"
 #include "logger/log.h"
+#include "glad/glad.h"
 
-#include <SDL/SDL.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL_opengl.h>
 
 struct PlatformSpecific
 {
-    // Nothing platform specific to be stored.
+    SDL_Window* window;
+    void* gl_context;
 };
 
 Video::~Video() = default; // For pimpl idiom.
@@ -18,12 +19,17 @@ Video::Video() : _timestep(60) {}; // For pimpl idiom.
 
 void Video::tear_down_gl()
 {
+    SDL_GL_DeleteContext(_platform_specific->gl_context);
+    SDL_DestroyWindow(_platform_specific->window);
     SDL_QuitSubSystem(SDL_INIT_VIDEO);
+
+    _platform_specific->window = nullptr;
+    _platform_specific->gl_context = nullptr;
 }
 
 void Video::swap_buffers() const
 {
-    SDL_GL_SwapBuffers();
+    SDL_GL_SwapWindow(_platform_specific->window);
 }
 
 bool Video::setup_gl()
@@ -32,55 +38,59 @@ bool Video::setup_gl()
 
     _platform_specific = std::make_unique<PlatformSpecific>();
 
-    if (SDL_InitSubSystem(SDL_INIT_VIDEO) < 0)
-    {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         log_error("SDL_Init Error: %s", SDL_GetError());
         SDL_ClearError();
         return false;
     }
 
-    SDL_ShowCursor(SDL_DISABLE);
-
     SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 5 );
-    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 5 );
+    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 6 );
     SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 5 );
 
-    SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 0);
-    SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 0);
-    SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE, 0);
-
-    SDL_GL_SetAttribute( SDL_GL_ACCUM_RED_SIZE, 0);
-    SDL_GL_SetAttribute( SDL_GL_ACCUM_GREEN_SIZE, 0);
-    SDL_GL_SetAttribute( SDL_GL_ACCUM_BLUE_SIZE, 0);
-    SDL_GL_SetAttribute( SDL_GL_ACCUM_ALPHA_SIZE, 0);
-
     SDL_GL_SetAttribute( SDL_GL_ACCELERATED_VISUAL, 1);
+    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1);
 
-    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+    SDL_GL_LoadLibrary(nullptr);
+    SDL_ClearError();
 
     //  Create a window
 
-    auto surface = SDL_SetVideoMode(480 * 2,
-                                    272 * 2,
-                                    0, // current display's bpp
-                                    SDL_DOUBLEBUF | SDL_OPENGL | SDL_SWSURFACE);
-
-    if (!surface)
+    _platform_specific->window = SDL_CreateWindow("SpelunkyPSP",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              480 * 2, 272 * 2,
+                              SDL_WINDOW_OPENGL |
+                              SDL_WINDOW_ALLOW_HIGHDPI);
+    if (!_platform_specific->window)
     {
-        log_error("SDL_SetVideoMode Error: %s", SDL_GetError());
+        log_error("SDL_CreateWindow Error: %s", SDL_GetError());
         SDL_ClearError();
         return false;
     }
 
-    if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress))
+    int w, h;
+    SDL_GL_GetDrawableSize(_platform_specific->window, &w, &h);
+    _viewport = std::make_shared<Viewport>(w, h);
+
+    _platform_specific->gl_context = SDL_GL_CreateContext(_platform_specific->window);
+    if (!_platform_specific->gl_context)
     {
+        log_error("SDL_GL_CreateContext Error: %s", SDL_GetError());
+        SDL_ClearError();
+        return false;
+    }
+
+    SDL_GL_MakeCurrent(_platform_specific->window, _platform_specific->gl_context);
+    SDL_ClearError();
+
+    if(!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress)) {
         log_error("Error while loading ptrs to OpenGL functions");
         return false;
     }
 
-    _viewport = std::make_shared<Viewport>(surface->w, surface->h);
-
     DebugGlCall(glEnable(GL_TEXTURE_2D));
+
     DebugGlCall(glShadeModel(GL_SMOOTH));
     DebugGlCall(glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE));
 
