a93f6858b2eacecd65c7e85c4120dbe509a6dbd1
Daniel 'dbeef' Zalega <daniel.zalega@gmail.com>
2024-06-16 05:56:50 +0200

    Cleanup of Windows build scripts

--- scripts/windows/config.ps1
+++ scripts/windows/config.ps1
@@ -1,22 +1,15 @@
-[CmdletBinding()]
-param (
-    [Parameter()][String]
-    $DepsDirectory="deps"
-)
-
 $ErrorActionPreference = "Stop"
 
-$ROOT = $(Get-Location)/../../
-$INSTALL_PATH = "$ROOT/tmp/install-windows"
-$DEPS_PATH = "$ROOT/$DepsDirectory/SDL"
+$ROOT_PATH = "$(Get-Location)/../../"
+$TMP_PATH = "$ROOT_PATH/tmp"
+$DEPS_PATH = "$TMP_PATH/deps-windows/SDL"
+$INSTALL_PATH = "$TMP_PATH/install-windows"
+$BUILD_PATH = "$TMP_PATH/build-windows"
 
-if (Test-Path "$ROOT/tmp/build-windows")
-{
-    Remove-Item -Force -Recurse "$ROOT/tmp/build-windows"
-}
-New-Item -Path "$ROOT/tmp/build-windows" -ItemType "Directory"
+New-Item -Path "$BUILD_PATH" -ItemType "Directory"
 
-cmake $ROOT `
-    -B"$ROOT/tmp/build-windows" `
+cmake `
+    -S $ROOT_PATH `
+    -B "$BUILD_PATH" `
     -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" `
     -DCMAKE_PREFIX_PATH="$DEPS_PATH"
