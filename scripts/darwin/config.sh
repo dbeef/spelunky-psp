5a6fc9f8965feedfd8da9fb732a3252d722d6f32
Daniel 'dbeef' Zalega <daniel.zalega@gmail.com>
2024-08-18 22:01:05 +0200

    Explicitly passing 'SPELUNKY_PSP_USE_VIRTUAL_FILESYSTEM' during project configuration

--- scripts/darwin/config.sh
+++ scripts/darwin/config.sh
@@ -6,5 +6,6 @@ cmake \
     -B $BUILD_PATH \
     -S $ROOT_PATH \
     -G "Unix Makefiles" \
+    -DSPELUNKY_PSP_USE_VIRTUAL_FILESYSTEM=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH
